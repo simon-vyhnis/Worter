ac59d6dd8c8a202b8936046c5e1f53e79444a766
Šimon Vyhnis <simon.vyhnis@gmail.com>
2021-12-12 18:39:57 +0100

    Created AboutFragment

--- app/src/main/java/com/simon/worter/MainActivity.kt
+++ app/src/main/java/com/simon/worter/MainActivity.kt
@@ -4,24 +4,14 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.viewModels
+import com.simon.worter.ui.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(item.itemId == R.id.statistics){
-
-            Toast.makeText(this, "Total words: ", Toast.LENGTH_LONG).show()
-        }
-        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/simon/worter/ui/StartFragment.kt
+++ app/src/main/java/com/simon/worter/ui/StartFragment.kt
@@ -1,15 +1,24 @@
 package com.simon.worter.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Button
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.simon.worter.R
 
 class StartFragment : Fragment() {
+    private val viewModel : MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root =  inflater.inflate(R.layout.fragment_start, container, false)
         val buttonPractice = root.findViewById<Button>(R.id.practice)
@@ -21,5 +30,20 @@ class StartFragment : Fragment() {
             it.findNavController().navigate(R.id.action_startFragment_to_addFragment)
         }
         return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.statistics){
+            viewModel.getNumberOfWords().observe(this) {
+                Toast.makeText(context, "Total words: $it", Toast.LENGTH_LONG).show()
+            }
+        }else if(item.itemId == R.id.about){
+            findNavController().navigate(R.id.action_startFragment_to_aboutFragment)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
